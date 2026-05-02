36acdfffc273936e2ad5ec3ed47656521d210614
CAOQUANGDU <cquangdu@gmail.com>
2026-05-02 17:15:41 +0000

    Day 2 with learning AI

---
+++ DAY_002/Readme.md
@@ -0,0 +1,8 @@
+# Simple Product Data Filtering Script
+
+This script uses the pandas library to read product data from a CSV file, filter for specific cripteria, such as electronics under 100$, and save the result to a new CSV file.
+
+## How to run
+
+'''bash
+python process_data.py
---
+++ DAY_002/process_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def folter_electronics_under_price(input_file, output_file, max_price):
+	df = pd.read_csv(input_file)
+	filered_df = df[
+		(df['category'] == 'Electronics') &
+		(df['prcice'] < max_prcie)
+	]
+
+	print(filtered_df)
+
+	filered_df.to_csv(output_file, index=False)
+
+filter_electronics_under_price(
+	input_file='products.csv',
+	output_file='filtered_electronics.csv',
+	max_price=100
+)
