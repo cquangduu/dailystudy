4ac18e998b0c0a01b301d3f92f5072f9a65f91a0
CAOQUANGDU <cquangdu@gmail.com>
2026-05-02 17:38:52 +0000

    fixed

--- DAY_002/process_data.py
+++ DAY_002/process_data.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 def filter_electronics_under_price(input_file, output_file, max_price):
 	df = pd.read_csv(input_file)
-	filered_df = df[
+	filtered_df = df[
 		(df['category'] == 'Electronics') &
-		(df['prcice'] < max_prcie)
+		(df['price'] < max_price)
 	]
 
 	print(filtered_df)
 
-	filered_df.to_csv(output_file, index=False)
+	filtered_df.to_csv(output_file, index=False)
 
 filter_electronics_under_price(
 	input_file='products.csv',
