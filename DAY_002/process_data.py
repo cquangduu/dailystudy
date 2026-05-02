365d1c67e0cfdc94c41456721f19bb0f06d1798c
CAOQUANGDU <cquangdu@gmail.com>
2026-05-02 17:32:12 +0000

    fix: process_data.py

--- DAY_002/process_data.py
+++ DAY_002/process_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def folter_electronics_under_price(input_file, output_file, max_price):
+def filter_electronics_under_price(input_file, output_file, max_price):
 	df = pd.read_csv(input_file)
 	filered_df = df[
 		(df['category'] == 'Electronics') &
